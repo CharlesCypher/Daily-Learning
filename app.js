303c9e8eff648b258a68c9737589465219d16fa3
CharlesCypher <charleschinagorom10@gmail.com>
2022-05-17 09:29:17 +1300

    changes

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const hamburger = document.querySelector(".hamburger");
 const dropDown = document.querySelector(".drop-down");
 const form = document.getElementById("formBox");
 const main = document.querySelector(".main");
+const footer = document.querySelector(".footer");
 
 hamburger.addEventListener("click", function () {
   form.classList.toggle("is-active");
   dropDown.classList.toggle("show");
-  main.classList.toggle("is-margin");
+  footer.classList.toggle("is-margin");
 });
 
 let title = document.getElementById("titleBox");
