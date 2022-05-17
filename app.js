c40d6e60e824cbd175233be11b475aa37ad4877e
CharlesCypher <charleschinagorom10@gmail.com>
2022-05-17 18:11:16 +0100

    changes

--- app.js
+++ app.js
@@ -18,6 +18,43 @@ let noteBody = document.querySelector(".note-body");
 let submit = document.getElementById("submitBtn");
 submit.addEventListener("click", (e) => {
   e.preventDefault;
-  noteTitle.textContent = titleInput.value;
+  nt = titleInput.value;
+  localStorage.setItem("title", nt);
+
+  const titleNote = localStorage.getItem("title");
+  noteTitle.textContent = titleNote;
   noteBody.textContent = textArea.value;
 });
+
+function storageAvailable(type) {
+  var storage;
+  try {
+    storage = window[type];
+    var x = "_storage_test_";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+
+if (storageAvailable("localStorage")) {
+  console.log("Yippee! We can use localStorage awesomeness");
+} else {
+  console.log("Too bad, no localStorage for us");
+}
